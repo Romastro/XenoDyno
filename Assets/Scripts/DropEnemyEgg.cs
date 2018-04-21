6e18f13f439cf9ee87a7637597ab7bc6f7471efe
Marco Giardina <marksson.dev@gmail.com>
2018-04-20 21:40:43 -0400

    Created scripts to spawn enemy randomly
    - Eggs are spawned between min and max ranges for all axis
    - Eggs drop and when colliding with the ground spawn selected enemies in editor
    
    Other: Spawn time interval, number of enemies, axis ranges for the egg spawning position, and egg prefab can be selected in editor.
    
    Drag the enemy egg spawner prefab in the scene and adjust paramters as needed.

---
+++ Assets/Scripts/DropEnemyEgg.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropEnemyEgg : MonoBehaviour
+{
+    public GameObject enemyToSpawn;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Ground")
+        {
+            Invoke("SpawnEnemy", 1);
+            Invoke("DestroyEgg", 1);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyToSpawn, transform.position, transform.rotation);
+    }
+
+    void DestroyEgg()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/RandomEggDrop.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEggDrop : MonoBehaviour
+{
+    public GameObject eggToDrop = null;
+    public int numOfEnemies = 0;
+    public float dropTimeInterval = 0;
+    private int enemyCounter = 0;
+
+    public float minX, maxX;
+    public float minY, maxY;
+    public float minZ, maxZ;
+
+    private Vector3 randSpawnVec3;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("DropEggRandomly", dropTimeInterval, 1);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (enemyCounter == numOfEnemies)
+        {
+            CancelInvoke("DropEggRandomly");
+        }
+    }
+
+    void DropEggRandomly()
+    {
+        Instantiate(eggToDrop, new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ)), eggToDrop.transform.rotation);
+        enemyCounter++;
+    }
+}
